5dd6f73b1d51689e883c7f5b6f134e00042f5cc9
Osman <osman_suastegui@hotmail.com>
2023-04-29 15:34:03 -0600

    refactorizacion

--- controladores/recuperarContra.php
+++ controladores/recuperarContra.php
@@ -3,31 +3,40 @@ include "../bayesian-poker/vistas/recuperarContra.html";
 include "../bayesian-poker/modelos/registrarCodigo.php";
 if (isset($_POST['email'])) {
     $email = $_POST['email'];
-//    /send_email_recuperar_contrasena
-    $url = 'http://localhost:5000/send_email_recuperar_contrasena';
     $codigo = rand(100000, 999999);
-    $data = array(
-        'destinatario' => $email,
-        "codigoDeValidacion" => $codigo
+
+    $regCod = new RegistrarCodigo();
+    $codigoRegistrado = $regCod->registrarCodigo($codigo, $email);
     
-    );
+    if($codigoRegistrado){
+        $url = 'http://localhost:5000/send_email_recuperar_contrasena';
+        $data = array(
+            'destinatario' => $email,
+            "codigoDeValidacion" => $codigo
+        
+        );
 
-    // Configurar la solicitud POST con cURL
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        // Configurar la solicitud POST con cURL
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 
-    $response = curl_exec($ch);
-    curl_close($ch);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        if($response == "1"){
+            echo "<div class='mensaje-exito'>Se ha enviado el correo exitosamente</div>";
 
-    if($response == "1"){
-        echo "<div class='mensaje-exito'>Se ha enviado el correo exitosamente</div>";
-        $regCod = new RegistrarCodigo();
-        $regCod->registrarCodigo($codigo, $email);
+        }else{
+            echo "<div class='mensaje-error'>El correo no se pudo enviar</div>";
+
+        }
     }else{
-        echo "<div class='mensaje-error'>El correo no se pudo enviar</div>";
+        echo "<div class='mensaje-exito'>Se ha enviado el correo exitosamente</div>";
+   
     }
+    
+    
 
 
   
--- modelos/registrarCodigo.php
+++ modelos/registrarCodigo.php
@@ -10,11 +10,17 @@ class RegistrarCodigo
         // obtener id 
         $sql = "SELECT idUsuario FROM usuarios WHERE correo = '$email'";
         $resultado = $con->query($sql);
-        $usu = $resultado->fetch_assoc();
-        $idUsuario = $usu['idUsuario'];
 
-        $sql = "INSERT INTO verificaciones (idUsuario, codigo, fechaCreacion, activo) VALUES ('$idUsuario', '$codigo', NOW(), '1')";
-        $resultado = $con->query($sql);
+        if($resultado->num_rows > 0){
+            $usu = $resultado->fetch_assoc();
+            $idUsuario = $usu['idUsuario'];
+    
+            $sql = "INSERT INTO verificaciones (idUsuario, codigo, fechaCreacion, activo) VALUES ('$idUsuario', '$codigo', NOW(), '1')";
+            $resultado = $con->query($sql);
+            return True;
+    
+        }
+        return False;
     }
 }
 ?>
